78c2a0690f32ab534b46ca8f4b7cfd536e9e28ec
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-09-24 22:08:48 -0500

    Hopefully this works, im struggling so hard on git.

---
+++ CarShow/src/model/ListCar.java
@@ -0,0 +1,72 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Cars")
+public class ListCar {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private int id;
+	@Column(name = "MAKE")
+	private String make;
+	@Column(name = "MODEL")
+	private String model;
+	@Column(name = "YEAR")
+	private String year;
+
+	public ListCar() {
+		super();
+	}
+
+	public ListCar(String make, String model, String year) {
+		super();
+		this.make = make;
+		this.model = model;
+		this.year = year;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String returnCarDetails() {
+		return make + ": " + model + ": " + year;
+	}
+
+}
